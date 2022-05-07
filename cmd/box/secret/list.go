dc174af35f32408f09a7269bd7bb176b67e413ec
mas2020 <61514041+mas2020@users.noreply.github.com>
2022-05-07 11:55:50 +0200

    (#12) add the '--items' flag to the 'secret ls' command

--- cmd/box/secret/list.go
+++ cmd/box/secret/list.go
@@ -6,6 +6,7 @@ package secret
 
 import (
 	"fmt"
+	"github.com/mas2020-golang/cryptex/packages/protos"
 	"github.com/mas2020-golang/cryptex/packages/utils"
 	"github.com/spf13/cobra"
 	"regexp"
@@ -14,6 +15,7 @@ import (
 
 var (
 	filter string
+	items  bool
 )
 
 // boxCmd represents the box command
@@ -21,7 +23,7 @@ var listCmd = &cobra.Command{
 	Use:     "list",
 	Aliases: []string{"ls"},
 	Short:   "List secret",
-	Long:    `List all the secret in the --box given flag. Use the flag --name
+	Long: `List all the secret in the --box given flag. Use the flag --name
 to filter using a regular expression.`,
 	Example: `$ cryptex secret ls --box test
 $ cryptex secret ls --box test --name '^secret.*test$`,
@@ -32,6 +34,7 @@ $ cryptex secret ls --box test --name '^secret.*test$`,
 
 func init() {
 	listCmd.Flags().StringVarP(&filter, "filter", "f", "", "The secret name as a regexp (e.g. 'test.*')")
+	listCmd.Flags().BoolVarP(&items, "items", "i", false, "Show the items' keys for the items saved into the secret")
 }
 
 func list() {
@@ -43,32 +46,46 @@ func list() {
 	utils.Check(err, "")
 	// ls the secret
 	fmt.Printf("%-15s%-10s%-31s%-31s%-11s%s\n", "NAME", "VERSION", "URL", "NOTES", "ITEMS", "LAST-UPD")
-	for _, s := range box.Secrets{
-		if len(s.Version) > 9{
-			s.Version =  s.Version[0:6] + "..."
+	for _, s := range box.Secrets {
+		if len(s.Version) > 9 {
+			s.Version = s.Version[0:6] + "..."
 		}
-		if len(s.Name) > 14{
-			s.Name =  s.Name[0:10] + "..."
+		if len(s.Name) > 14 {
+			s.Name = s.Name[0:10] + "..."
 		}
-		if len(s.Url) > 30{
-			s.Url =  s.Url[0:26] + "..."
+		if len(s.Url) > 30 {
+			s.Url = s.Url[0:26] + "..."
 		}
 		s.Url = utils.BlueS(fmt.Sprintf("%-31s", s.Url))
-		if len(s.Notes) > 30{
-			s.Notes =  s.Notes[0:26] + "..."
+		if len(s.Notes) > 30 {
+			s.Notes = s.Notes[0:26] + "..."
 		}
-		s.Name = utils.LightRedS(utils.BoldS(fmt.Sprintf("%-15s", s.Name)))
+		s.Name = utils.RedS(utils.BoldS(fmt.Sprintf("%-15s", s.Name)))
 		lastUpdated := s.LastUpdated.AsTime().In(loc).Format("Jan 2 15:04 2006 MST")
 		// check the name flag
-		if len(filter) > 0{
+		if len(filter) > 0 {
 			r, _ := regexp.Compile(filter)
-			if r.MatchString(s.Name){
+			if r.MatchString(s.Name) {
 				fmt.Printf("%s%-10s%s%-31s%-11d%s\n", s.Name, s.Version, s.Url, s.Notes, len(s.Others),
 					lastUpdated)
+				showItems(s)
 			}
-		}else{
+		} else {
 			fmt.Printf("%s%-10s%-31s%-31s%-11d%s\n", s.Name, s.Version, s.Url, s.Notes, len(s.Others),
 				lastUpdated)
+			showItems(s)
+		}
+	}
+}
+
+func showItems(s *protos.Secret) {
+	if !items {
+		return
+	}
+	if s.Others != nil && len(s.Others) > 0 {
+		//fmt.Println(" - items:")
+		for k, _ := range s.Others {
+			fmt.Printf("%-1s.%s\n","", utils.BoldS(k))
 		}
 	}
 }
