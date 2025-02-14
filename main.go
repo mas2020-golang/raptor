9376f94f686186c572e3bb45cc31daed4374bee2
mas2020 <andrea.genovesi@gmail.com>
2025-02-14 16:25:55 +0100

    fix: error message is doubled (#73)
    fix the creation of the box is failing when --force is given and the CRYPTEX FOLDER env variable is pointing to a null folder

--- main.go
+++ main.go
@@ -1,10 +1,13 @@
 /*
 Copyright © 2022 NAME HERE <EMAIL ADDRESS>
-
 */
 package main
 
 import (
+	"log/slog"
+	"os"
+	"strings"
+
 	"github.com/mas2020-golang/cryptex/cmd"
 	"github.com/mas2020-golang/cryptex/packages/utils"
 )
@@ -12,6 +15,33 @@ import (
 var GitCommit string
 
 func main() {
+	// Retrieve the log level from the environment variable
+	logLevel := getLogLevelFromEnv()
+
+	// Create a new logger with the specified log level
+	logger := slog.New(slog.NewTextHandler(os.Stdout, &slog.HandlerOptions{
+		Level: logLevel,
+	}))
+	// set the default logger for the whole application
+	slog.SetDefault(logger)
 	utils.GitCommit = GitCommit
 	cmd.Execute()
+}
+
+// getLogLevelFromEnv retrieves the log level from the RAPTOR_LOGLEVEL environment variable.
+// Defaults to Info level if the variable is not set or contains an invalid value.
+func getLogLevelFromEnv() slog.Level {
+	levelStr := strings.ToLower(os.Getenv("RAPTOR_LOGLEVEL"))
+	switch levelStr {
+	case "debug":
+		return slog.LevelDebug
+	case "info":
+		return slog.LevelInfo
+	case "warn":
+		return slog.LevelWarn
+	case "error":
+		return slog.LevelError
+	default:
+		return slog.LevelError
+	}
 }
