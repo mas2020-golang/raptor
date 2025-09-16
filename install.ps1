de2bcbb4d24d9687de6ff485fc50ab373c25c877
Andrea Genovesi <andrea.genovesi@gmail.com>
2025-09-16 18:55:59 +0200

    feat: add the installation script for Windows

---
+++ install.ps1
@@ -0,0 +1,8 @@
+# install a specific version tag
+.\install.ps1 -Version v0.3.0
+
+# install to a custom folder
+.\install.ps1 -InstallDir "C:\Tools\raptor"
+
+# overwrite existing binary
+.\install.ps1 -Force
